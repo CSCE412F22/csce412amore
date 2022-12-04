2cd1a6469a8f841c7dbab40e64547e05491251be
Abhishek <abhimore74@gmail.com>
2022-12-03 19:42:21 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+//TODO: add some documentation tings
+function App() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
